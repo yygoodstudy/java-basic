167bb1f51e8570a8e0a91c76fcec829b5100f4ce
yaoyuan <1601206019@qq.com>
2022-08-26 00:53:21 +0800

    java8 lambda demo third commit --[yaoyuan]

--- src/com/yy/java8/lambda/Java8FunctionalInterfaceTest.java
+++ src/com/yy/java8/lambda/Java8FunctionalInterfaceTest.java
@@ -1,8 +1,11 @@
 package com.yy.java8.lambda;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 /**
@@ -24,11 +27,31 @@ public class Java8FunctionalInterfaceTest {
         return numbers;
     }
 
+    // Function<R,T>
+    public static String apply(String str, Function<String,String> function){
+        return function.apply(str);
+    }
+
+    public static List<String> stringFilter(List<String> list, Predicate<String> predicate){
+        List<String> strings = new ArrayList<>();
+        for (String s : list) {
+            if (predicate.test(s)) {
+                strings.add(s);
+            }
+        }
+        return strings;
+    }
+
     public static void main(String[] args) {
         happy(1000d, d -> System.out.println("消费：" + d + "元"));
 
         List<Integer> list = generate(5, () -> (int) (Math.random() * 10));
         list.forEach(System.out::println);
 
+        String apply = apply(" hello-world ", s -> s.toUpperCase().trim());
+        System.out.println("apply = " + apply);
+
+        List<String> strings = stringFilter(Arrays.asList("he", "ha", "hehe", "haha", "heihei"), x -> x.length() >= 3);
+        System.out.println(strings);
     }
 }
